164cd1d66e198f8fbdf2aa17ed1573e87a7164ff
Siyuan Wan <wsytj@outlook.com>
2021-07-26 09:52:13 +0800

    print y

---
+++ aihub2021/linear_regression.py
@@ -0,0 +1,14 @@
+from sklearn.linear_model import LinearRegression
+import numpy as np
+
+x = np.linspace(0, 1)
+y = 3 * x + 4 + np.random.rand() / 100
+x = x.reshape(-1, 1)
+
+model = LinearRegression()
+model.fit(x, y)
+
+print(model.coef_)
+print(model.intercept_)
+print(f'y = {model.coef_[0]} * x + {model.intercept_}')
+print("y")
